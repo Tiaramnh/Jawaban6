5d18536dd2f01109e81c5b1b70c2dac8bdca1cca
Tiara332 <148456031+Tiaramnh@users.noreply.github.com>
2023-11-17 05:56:21 +0700

    Jawaban UTS No.6

---
+++ Variabel.java
@@ -0,0 +1,9 @@
+public class Variabel {
+  public static void main(String[] args) {
+    String namaSaya = "Mutiara Nur Handayani";
+    int tahunlahir = 2003;
+
+    System.out.println("Nama saya   :" + namaSaya);
+    System.out.println("Tahun lahir :" + tahunlahir);
+  }
+}
